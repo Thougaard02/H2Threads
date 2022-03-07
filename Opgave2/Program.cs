1eedfd7b937a7c856dcc4b32de837ee8d02d5e66
Rasmus <Rasmus@vinperlen.dk>
2022-03-07 20:43:00 +0100

    Opgave 2 done

---
+++ Opgave2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Opgave2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadOutput thread = new ThreadOutput();
+
+            Thread star = new Thread(thread.PrintStar);
+            Thread hashtag = new Thread(thread.PrintHashTag);
+
+            star.Start();
+            hashtag.Start();
+
+            star.Join();
+            hashtag.Join();
+        }
+    }
+}
---
+++ Opgave2/ThreadOutput.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Opgave2
+{
+    public class ThreadOutput
+    {
+        static int linesum;
+        static object _lock = new object();
+        public void PrintStar()
+        {
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(_lock);
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("*");
+                        Interlocked.Increment(ref linesum);
+                    }
+                    Console.Write($" {linesum}\n");
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void PrintHashTag()
+        {
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(_lock);
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("#");
+                        Interlocked.Increment(ref linesum);
+                    }
+                    Console.Write($" {linesum}\n");
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
